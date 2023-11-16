b8a8dc3747cad8c1eaf1b7a8c098c97f612b8e2d
white-jared <jaredm0520@gmail.com>
2023-11-16 01:54:51 -0800

    Create lbs.py

---
+++ cs457-final-project/lbs.py
@@ -0,0 +1,13 @@
+# Jared White
+# Python code for the library mangement system
+# Utilizes the pscycopg library for connection with the PostgreSQL database
+
+import psycopg
+from psycopg import sql
+import sys
+
+def main():
+    return
+
+if __name__ == '__main__':
+    main()
